cbc3da72ecb19e546d7ffcc3af996c31da72e241
OsarodionOdion <osarodionogbeborevans@yahoo.com>
2026-09-22 11:52:52 -0400

    sentiment_analysis: comments and descriptions added to function

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -9,11 +9,29 @@ Input json: { "raw_document": { "text": text_to_analyse } }
 
 '''
 
+import requests
+
 def sentiment_analyzer(text_to_analyse):
+
+    """ Function for running sentiment analysis using the Watson NLP BERT Sentiment Analysis function.
     
+    Args:
+        text_to_analyse (str): text to be analyzed
+
+    returns:
+        str: text attribute of the BERT model response object on the analyzed text
+
+    """
+    # URL of the sentiment analysis service
     url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
+    
+    # header required for the API request
     header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
+    
+    # dictionary with the text to be analyzed
     myobj = { "raw_document": { "text": text_to_analyse } }
+
+    # send POST request to the API with the text and headers
     response = requests.post(url, json = myobj, headers=header)
 
     return response.text
